91b6551048ac18cff2389916abb04ccf1570cb29
jocosep121 <jocosep578@ikanid.com>
2024-02-14 23:40:45 +0200

    eufhqweui

--- script.js
+++ script.js
@@ -482,15 +482,16 @@ function Load() {
 
         all_div1.addEventListener("click", function () {
             object_id = i;
+            console.log(object_id);
             openOffCanva();
         });
 
     }
-    let Button = document.getElementById('FaveBtn');
+    /*let Button = document.getElementById('FaveBtn');
     Button.addEventListener("click", function () {
         list.push({ object_id });
         console.log(object_id);
-    });
+    });*/
 
 }
 function FilterDes() {
@@ -946,56 +947,65 @@ function openVideo() {
 
 }
 
+function GetFave() {
+    list.push(object_id);
+    console.log(list[0]);
+}
+
 function LoadFave() {
     let recipe = document.getElementById('FaveContainer');
 
-    let length = recipes.length;
+    let length = list.length;
     let current = list[0];
+    console.log(recipes[list[0]].name);
+    console.log(current);
+
 
     for (let i = 0; i < length; i++) {
-        if (current == i) {
-            let all_div1 = document.createElement("div");
-            all_div1.classList.add("col", "px-4");
-            all_div1.setAttribute("id", recipes[i].id);
-            recipe.appendChild(all_div1);
 
-            let all_div2 = document.createElement("div");
-            all_div2.classList.add("card", "h-100", "border", "border-success-subtle");
-            all_div2.onmouseover = function () {
-                all_div2.style.boxShadow = "0 .5rem 1rem #1E4847";
-                all_div2.style.borderStyle = "hidden";
-                all_div2.style.transitionDuration = "0.75s";
-            }
+        let all_div1 = document.createElement("div");
+        all_div1.classList.add("col", "px-4");
+        all_div1.setAttribute("id", recipes[list[i]].id);
+        recipe.appendChild(all_div1);
+        console.log(current);
 
-            all_div2.onmouseleave = function () {
-                all_div2.style.boxShadow = "none";
-                all_div2.borderStyle = "solid";
-                all_div2.transitionDuration = "0.75s";
-            }
-            all_div1.appendChild(all_div2);
-
-            let all_image = document.createElement("img");
-            all_image.classList.add("card-img-top");
-            all_image.src = recipes[i].image;
-            all_div2.appendChild(all_image);
-
-            let all_div3 = document.createElement("div");
-            all_div3.classList.add("card-body");
-            all_div3.style.backgroundColor = "#A0E8AF";
-            all_div2.appendChild(all_div3);
-
-            let all_h = document.createElement("h5");
-            all_h.classList.add("card-title");
-            all_h.innerText = recipes[i].name;
-            all_div3.appendChild(all_h);
-
-            all_div1.addEventListener("click", function () {
-                object_id = i;
-                openOffCanva();
-            });
-
-            current++;
+        let all_div2 = document.createElement("div");
+        all_div2.classList.add("card", "h-100", "border", "border-success-subtle");
+        all_div2.onmouseover = function () {
+            all_div2.style.boxShadow = "0 .5rem 1rem #1E4847";
+            all_div2.style.borderStyle = "hidden";
+            all_div2.style.transitionDuration = "0.75s";
         }
+
+        all_div2.onmouseleave = function () {
+            all_div2.style.boxShadow = "none";
+            all_div2.borderStyle = "solid";
+            all_div2.transitionDuration = "0.75s";
+        }
+        all_div1.appendChild(all_div2);
+
+        let all_image = document.createElement("img");
+        all_image.classList.add("card-img-top");
+        all_image.src = recipes[[list[i]]].image;
+        all_div2.appendChild(all_image);
+
+        let all_div3 = document.createElement("div");
+        all_div3.classList.add("card-body");
+        all_div3.style.backgroundColor = "#A0E8AF";
+        all_div2.appendChild(all_div3);
+
+        let all_h = document.createElement("h5");
+        all_h.classList.add("card-title");
+        all_h.innerText = recipes[list[i]].name;
+        all_div3.appendChild(all_h);
+
+        all_div1.addEventListener("click", function () {
+            object_id = i;
+            openOffCanva();
+        });
+       // console.log(current)
+        current++;
+
 
     }
 
